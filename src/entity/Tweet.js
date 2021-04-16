304cd364211fe841fb0104241dde0af9caf427da
markhaur <fnisar394@protonmail.com>
2021-04-16 10:16:59 +0500

    completed example for typeorm in javascript. it was really fun ;)

--- src/entity/Tweet.js
+++ src/entity/Tweet.js
@@ -22,24 +22,3 @@ module.exports = {
         }
     }
 }
-
-
-// export class Tweet {
-
-//     @PrimaryGeneratedColumn()
-//     id: string;
-
-//     @Column({ type: "varchar", length: 80 })
-//     title: string;
-
-//     @Column({ type: "varchar", length: 300})
-//     content: string;
-
-//     /**
-//      * Eager loading happens if user: User
-//      * we can make it Lazy loading by replacing user: User with 
-//      * user: Promise<User>
-//      */
-//     @ManyToOne(type => User, user => user.tweets)
-//     user: Promise<User>;
-// }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-// import "reflect-metadata";
-// import {createConnection} from "typeorm";
-// import { Bootstrap, find } from "./bootstrap";
-// import {User} from "./entity/User";
-
 const typeorm = require('typeorm');
 const bootstrap = require('./bootstrap');
+var EntitySchema = typeorm.EntitySchema;
 
-typeorm.createConnection().then(async connection => {
+typeorm.createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "learning",
+    password: "learning",
+    database: "social_network",
+    synchronize: true,
+    entities: [
+        new EntitySchema(require('./entity/User')),
+        new EntitySchema(require('./entity/Tweet'))
+    ]
+}).then(async connection => {
+    var user = {
+        firstName: 'munsif',
+        lastName: 'ali',
+        age: 25
+    }
 
-    await bootstrap.Bootstrap(connection).catch(err => console.log(err));
-    await bootstrap.find(connection).catch(err => console.log(err));
+    var tweet = {
+        title: 'This is munsifali tweet',
+        content: 'This tweet is done during typeorm js example',
+        user: user
+    }
 
-}).catch(error => console.log(error));
+    var userRepo = connection.getRepository("User");
+    userRepo.save(user)
+        .then(savedUser => {
+            console.log("post have been saved: ", savedUser);
+        })
+        .catch(err => {
+            console.log('received error while saving user: ', err);
+        });
+
+    var tweetRepo = connection.getRepository("Tweet");
+    tweetRepo.save(tweet)
+        .then(savedTweet => {
+            console.log('tweet have been saved: ', savedTweet);
+        })
+        .catch(err => {
+            console.log('received error while saving tweet: ', err);
+        })
+}).catch(err => {
+    console.log('error while creating connection: ', err);
+})
